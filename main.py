5fb3b096993145be44b47832c038caa3ced09bbd
alucardthefish <sergiop18@gmail.com>
2022-06-27 01:12:49 -0500

    Refactor code
    Add Application factory pattern
    Configure celery to work with the pattern
    Manage changes to the database with SQLAlchemy and Alembic

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-from fastapi import FastAPI
-from celery import Celery
+from project import create_app
 
 
-app = FastAPI()
-
-
-celery = Celery(
-    __name__,
-    broker="redis://127.0.0.1:6379/0",
-    backend="redis://127.0.0.1:6379/0"
-)
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello world"}
-
-
-@celery.task
-def divide(x, y):
-    import time
-    time.sleep(5)
-    return x / y
+app = create_app()
+celery = app.celery_app
