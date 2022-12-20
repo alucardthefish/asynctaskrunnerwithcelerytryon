3d725bb939d3d2f24581e9ac4f1beddf12b93ccc
alucardthefish <sergiop18@gmail.com>
2022-12-19 21:23:04 -0500

    Add multiply task function with a delay of 25 seconds

--- project/users/tasks.py
+++ project/users/tasks.py
@@ -6,3 +6,10 @@ def divide(x, y):
     import time
     time.sleep(10)
     return x / y
+
+@shared_task
+def multiply(x, y):
+    import time
+    time.sleep(25)
+    result = x * y
+    return f"The result of {x} times {y} is : {result}"
