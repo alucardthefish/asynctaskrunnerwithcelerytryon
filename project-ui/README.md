f4a6d8099b503b96ccfe85d35940e6ef5a122f3c
alucardthefish <sergiop18@gmail.com>
2023-12-03 20:00:19 -0500

    Add user endpoints, update angular code and readme file

--- project-ui/README.md
+++ project-ui/README.md
@@ -25,3 +25,7 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Node info
+
+it is using Node v14.17.6 (If using nvm just check with nvm ls, then nvm use VERSION) this might need sudo/admin privileges.
--- project/users/__init__.py
+++ project/users/__init__.py
@@ -1,8 +1,27 @@
 from fastapi import APIRouter
+from project.database import SessionLocal
 
 
 users_router = APIRouter(
-    prefix="/users",
+    prefix="/user",
 )
+db = SessionLocal()
 
 from . import models, tasks # noqa
+
+@users_router.get("/", status_code=200)
+def get_users():
+    users = db.query(models.User).all()
+    return users
+
+@users_router.post("/")
+def create_user(user_name: str, email: str):
+    new_user = models.User(user_name, email)
+    db.add(new_user)
+    db.commit()
+    return new_user
+
+@users_router.get("/summup/", status_code=200)
+async def get_summation():
+    tasks.sumup.delay(32, 49)
+    return {"message": "Summation task is executing..."}
